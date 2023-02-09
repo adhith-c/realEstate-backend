dc49d4b28c288e64010e401a2c08b4c0649e16d0
adhith-c <adhithadhi17@gmail.com>
2023-02-09 17:26:59 +0530

    allowed origins updated

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
   process.env.FRONTENDURL,
-  process.env.BACKENDURL,
   "http://127.0.0.1:5500",
-  "http://localhost:4000",
+  process.env.BACKENDURL,
+  "http://localhost:3000",
 ];
 
 module.exports = allowedOrigins;
